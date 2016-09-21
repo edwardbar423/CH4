250a7a76309dddf97f371772a2c6801c53a9d7d5
Andrew Barber <edwardbar423@gmail.com>
2016-09-20 22:18:00 -0400

    Finished Chapter 4

--- World Trotter/ViewController.swift
+++ World Trotter/ViewController.swift
@@ -15,15 +15,7 @@ class ViewController: UIViewController {
         
         
         // let or var | name | : | type | = value
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blue
-        self.view .addSubview(firstView)
-        
-        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
-        let secondView = UIView(frame:secondFrame)
-        secondView.backgroundColor = UIColor.green
-        firstView.addSubview(secondView)
+    
         
     }
 
