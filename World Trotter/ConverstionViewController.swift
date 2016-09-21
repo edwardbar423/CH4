8496e9326ad2a5ed0447b24699f8114e55aa0769
Andrew Barber <edwardbar423@gmail.com>
2016-09-21 00:08:23 -0400

    Still working, finished fahrenheit calculation.

--- World Trotter/ConverstionViewController.swift
+++ World Trotter/ConverstionViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ConverstionViewController: UIViewController {
 
+    //Variables here, things that store data
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    
+    @IBOutlet var fahrenheitField: UITextField!
+    
+    var fahrenheitValue : Double = 0.0
+    var celsiusValue : Double = 0.0
+    
+    //view lifecycle starts here.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,6 +35,35 @@ class ConverstionViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //Actions
+    
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ sender: UITextField) {
+        
+        if let text = sender.text, let value = Double(text) {
 
+            self.fahrenheitValue = value
+            self.celsiusValue = convertedFahrenheit(value: self.fahrenheitValue)
+            updateCelsiusLabel()
+        }
+    }
+
+    @IBAction func dismissKeyboard(_ sender: AnyObject) {
+        self.fahrenheitField.resignFirstResponder()
+    }
+    //Conversion Methods
+    
+    func convertedCelsius(value: Double) -> Double {
+        return value - 32 * (5.0 / 9.0)
+    }
+    
+    func convertedFahrenheit(value: Double) -> Double {
+        return value * (9.0/5.0) + 32
+    }
+    
+    func updateCelsiusLabel() {
+        self.celsiusLabel.text = "\(self.celsiusValue)"
+    }
+    
 }
 
