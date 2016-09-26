f3cce03f00d29194f1626fc85db1672a0cf8a52d
Andrew Barber <edwardbar423@gmail.com>
2016-09-25 22:42:16 -0400

    Finished Chapter 7

--- World Trotter/MapViewController.swift
+++ World Trotter/MapViewController.swift
@@ -8,8 +8,43 @@
 
 import UIKit
 import Foundation
+import MapKit
+
 
 class MapViewController : UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        
+        mapView = MKMapView()
+        
+        view = mapView
+    
+        
+        let segItems : Array = ["Standard", "Hybrid", "Satellite"]
+        let segControl : UISegmentedControl = UISegmentedControl(items: segItems)
+        
+        segControl.backgroundColor = UIColor.init(white: 1.0, alpha: 0.5)
+        segControl.selectedSegmentIndex = 0
+        
+        segControl.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(segControl)
+        
+        segControl.addTarget(self, action: #selector(mapTypeChanged), for: UIControlEvents.valueChanged)
+        
+        let segControlTopConstraint = segControl.topAnchor.constraint(equalTo: self.topLayoutGuide.bottomAnchor, constant : 8)
+        let margins : UILayoutGuide = self.view.layoutMarginsGuide
+        let leadingConstraint = segControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        segControlTopConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        
+    }
+    
     
     override func viewDidLoad () {
         
@@ -18,10 +53,25 @@ class MapViewController : UIViewController {
         print("Hello darkness my old friend")
         
     }
-    
+ 
+    func mapTypeChanged(sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.mapView.mapType = MKMapType.standard
+            break
+        case 1:
+            self.mapView.mapType = MKMapType.hybrid
+            break
+        case 2:
+            self.mapView.mapType = MKMapType.satellite
+        default:
+            break
+            
+        }
+        
+    }
 }
-
-
 
 
 
